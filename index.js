aee5275f129a84cbe0504f4d43cc516691785661
5106139github <mathyamlava143@gmail.com>
2024-08-13 11:55:04 +0530

    episode-01

---
+++ index.js
@@ -0,0 +1,10 @@
+const heading = React.createElement(
+  "div",
+  { className: "parent" },
+  React.createElement("div", { className: "child" }, [
+    React.createElement("h1", {}, "this is my h1 tag"),
+    React.createElement("h2", {}, "mt h2 tag"),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
